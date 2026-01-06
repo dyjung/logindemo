ad789ff556720528eaa0fcb6b4b1f15c3eea05a0
dyjung <nineheadulp@gmail.com>
2026-01-06 19:02:50 +0900

    Initial Commit

---
+++ LoginDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginDemo
+//
+//  Created by Doyoung Chung on 1/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginDemo/LoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginDemoApp.swift
+//  LoginDemo
+//
+//  Created by Doyoung Chung on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
